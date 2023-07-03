69819727529549d6fc5001399908864e21c467f0
Curtis <curtis.turk@hl.co.uk>
2023-07-03 15:44:06 +0100

    project setup 🎉

---
+++ firstProject/firstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstProjectApp.swift
+//  firstProject
+//
+//  Created by Curtis Turk on 03/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct firstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
